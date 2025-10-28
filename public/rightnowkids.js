c59d259ce6fbc46941c1558efa1f13884bc1484b
megankvalvik <megankvalvik@rightnowmedia.org>
2025-10-28 16:54:06 -0500

    Update rightnowkids.js

--- public/rightnowkids.js
+++ public/rightnowkids.js
@@ -2,21 +2,10 @@ import { setupGlobal } from './components/global.js';
 import { setupPopups } from './components/popups.js';
 import { setupForms } from './components/forms.js';
 
-console.log('rightnowpastors loaded');
+console.log('rightnowkids loaded');
 
 setupGlobal();
 setupPopups();
 setupForms();
 
 
-//////////// Success Pages ////////////
-
-const path = window.location.pathname;
-
-// run only on specific success URLs
-const SUCCESS_ROUTES = {
-  '/success/plus-us-calendly': selfSchedulePPlusUSSuccess
-};
-
-const run = SUCCESS_ROUTES[path];
-if (run) run();
