8fe80da0189b14f84ac0c82243aae000aa91655c
megankvalvik <megankvalvik@rightnowmedia.org>
2026-04-02 09:58:41 -0500

    Update rightnowkids.js

--- public/rightnowkids.js
+++ public/rightnowkids.js
@@ -1,6 +1,7 @@
 import { setupGlobal } from './components/global.js';
 import { setupPopups } from './components/popups.js';
 import { setupForms } from './components/forms.js';
+import { selfScheduleKids } from './components/selfschedule_kids.js';
 
 console.log('rightnowkids loaded');
 
